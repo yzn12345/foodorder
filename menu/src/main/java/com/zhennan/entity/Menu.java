204c2e7e0adf8898d0b4a976c443489986d950e1
Zhennan <yzhenn5@vt.edu>
2021-02-26 22:33:39 -0500

    Initial commit

---
+++ menu/src/main/java/com/zhennan/entity/Menu.java
@@ -0,0 +1,13 @@
+package com.zhennan.entity;
+
+import lombok.Data;
+
+//实体类
+@Data
+public class Menu {
+    private long id;
+    private String name;
+    private double price;
+    private String flavor;
+
+}
---
+++ menu/src/main/java/com/zhennan/repository/MenuRepository.java
@@ -0,0 +1,17 @@
+package com.zhennan.repository;
+
+import com.zhennan.entity.Menu;
+
+import java.util.List;
+
+public interface MenuRepository {
+
+    public List<Menu> findAll(int index, int limit);
+    public int count();
+    public Menu findById(long id);
+    public void save(Menu menu);
+    public void update(Menu menu);
+    public void deleteById(long id);
+
+
+}
